55723cc097b43b013f932bc30d90b7227bc9202a
blood <hey-blood@foxmail.com>
2021-06-02 17:41:59 +0800

    添加类构造函数

--- src/example/myapp/Aquarium.kt
+++ src/example/myapp/Aquarium.kt
@@ -1,9 +1,32 @@
 package example.myapp
 
-class Aquarium {
+class Aquarium(length: Int = 100, width: Int = 20, height: Int = 40) {
+    init {
+        println("aquarium initializing")
+    }
+
+    init {
+        // 1 liter = 1000 cm^3
+        println("Volume: ${width * length * height / 1000} l")
+    }
+
+    constructor(numberOfFish: Int) : this() {
+        // 2,000 cm^3 per fish + extra room so water doesn't spill
+        val tank = numberOfFish * 2000 * 1.1
+        // calculate the height needed
+        height = (tank / (length * width)).toInt()
+    }
+
+    // Dimensions in cm
     var width: Int = 20
     var height: Int = 40
     var length: Int = 100
+
+    var volume: Int
+        get() = width * height * length / 1000
+        set(value) {
+            height = (value * 1000) / (width * length)
+        }
 
     fun printSize() {
         println("Width: $width cm " +
--- src/example/myapp/main.kt
+++ src/example/myapp/main.kt
@@ -1,11 +1,31 @@
 package example.myapp
 
 fun buildAquarium() {
-    val myAquarium = Aquarium()
-    myAquarium.printSize()
+    val aquarium1 = Aquarium()
+    aquarium1.printSize()
+    // default height and length
+    val aquarium2 = Aquarium(width = 25)
+    aquarium2.printSize()
+    // default width
+    val aquarium3 = Aquarium(height = 35, length = 110)
+    aquarium3.printSize()
+    // everything custom
+    val aquarium4 = Aquarium(width = 25, height = 35, length = 110)
+    aquarium4.printSize()
 
-    myAquarium.height = 60
-    myAquarium.printSize()
+    // use secondary constructors
+    println("========== aquarium6 ==========")
+    val aquarium6 = Aquarium(numberOfFish = 29)
+    aquarium6.printSize()
+    println("Volume: ${aquarium6.width * aquarium6.length * aquarium6.height / 1000} l")
+
+    println("========== aquarium7 ==========")
+    val aquarium7 = Aquarium(numberOfFish = 29)
+    aquarium7.printSize()
+    println("Volume: ${aquarium7.width * aquarium7.length * aquarium7.height / 1000} l")
+    aquarium7.volume = 70
+    aquarium7.printSize()
+    println("Volume: ${aquarium7.width * aquarium7.length * aquarium7.height / 1000} l")
 }
 
 fun main() {
