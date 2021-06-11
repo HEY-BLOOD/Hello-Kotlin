95490d9c0344ce678894b64491b3b757b968085c
blood <hey-blood@foxmail.com>
2021-06-11 12:34:18 +0800

    使用接口委托

--- src/example/myapp/AquariumFish.kt
+++ src/example/myapp/AquariumFish.kt
@@ -1,23 +1,30 @@
 package example.myapp
 
-abstract class AquariumFish {
-    abstract val color: String
-}
-
 interface FishAction {
     fun eat()
 }
 
-class Shark : AquariumFish(), FishAction {
+class Shark : FishAction, FishColor {
     override val color = "gray"
     override fun eat() {
         println("hunt and eat fish")
     }
 }
 
-class Plecostomus : AquariumFish(), FishAction {
+object GoldColor : FishColor {
     override val color = "gold"
+}
+
+class PrintingFishAction(val food: String) : FishAction {
     override fun eat() {
-        println("eat algae")
+        println(food)
     }
 }
+
+interface FishColor {
+    val color: String
+}
+
+class Plecostomus(fishColor: FishColor = GoldColor) :
+        FishAction by PrintingFishAction("eat algae"),
+        FishColor by fishColor
